71b0b48aa088cea682136bb6e815f4faa4d5c0e8
Remon <remon.onnorokom@gmail.com>
2025-12-21 12:07:45 +0600

    Add(+) ClientHandle class

---
+++ C#/System component/ThreadPool/ThreadPool/ClientHandle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadPool
+{
+    public class ClientHandle
+    {
+        public Guid ID;
+        public bool IsSimpleTask = false;
+    }
+}
