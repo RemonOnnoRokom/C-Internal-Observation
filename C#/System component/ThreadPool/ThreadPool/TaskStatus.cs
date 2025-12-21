2164a4c559c914d87abac45fdb270be2945cd66b
Remon <remon.onnorokom@gmail.com>
2025-12-21 12:10:02 +0600

    Add(+) TaskStatus class

---
+++ C#/System component/ThreadPool/ThreadPool/TaskStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadPool
+{
+    public delegate void UserTask();
+    public class TaskStatus
+    {
+        public bool Success = true;
+        public Exception InnerException = null;
+    }
+}
